147f7d39c57761a940b49ff1cf988d815867f1b3
John Osorio <johnosorio@Johns-MacBook-Pro.local>
2018-05-09 08:06:44 -0500

    Show frame by frame after press a key. With enter exits

--- src/common.cpp
+++ src/common.cpp
@@ -5,9 +5,21 @@ using namespace cv;
 
 int main(int argc, char **argv){
     if (argc!=2){
-        printf("You need to provide an argument: video");
+        printf("You need to provide an argument: video\n");
     }
     VideoCapture capture(argv[1]);
-    if()
+    if(!capture.isOpened()){
+	printf("Video doesn't exists\n");
+    }
+    namedWindow("frame",CV_WINDOW_AUTOSIZE);
+    Mat frame;
+    for(;;){
+	capture>>frame;
+	if(frame.empty()) break;
+	imshow("frame",frame);
+	if(waitKey(0) == 27) break;
+    }
     capture.release();
+    destroyAllWindows();
+    return 0;
 }
