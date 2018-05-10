67c6c25626a3c636e2ecd0f8ab4179082b05e0b7
John Osorio <johnosorio@Johns-MacBook-Pro.local>
2018-05-10 10:05:01 -0500

    data calculation

--- src/common.cpp
+++ src/common.cpp
@@ -3,6 +3,8 @@
 
 static const int INPUT_W = 500;
 static const int INPUT_H = 375;
+static const int INPUT_C = 3;
+static const int N = 1;
 
 using namespace cv;
 
@@ -10,12 +12,33 @@ int preprocessFrame(Mat frame, Mat& frame_scaled){
     
     Size size(INPUT_W,INPUT_H);
 
+    //In this pointer we are going to put the info of each frame
+    //in order to process it and feed the FasterRCNN example 
+    float* data = new float[N*INPUT_C*INPUT_H*INPUT_W];
+    //Pixel mean used by FasterRCNN authors. Also in BGR order
+    float pixelMean[3] = { 102.9801f, 115.9465f, 122.7717f }; 
+    
     if(frame.empty()) return -1;
 
     if(frame.channels() == 4)
         cvtColor(frame, frame, COLOR_BGRA2BGR);
     resize(frame, frame_scaled, size);
-    
+    Mat_<Vec3f>::iterator it;
+    unsigned int volChl = INPUT_H*INPUT_W;
+    for (int c = 0; c < INPUT_C; ++c)                              
+    {
+        //Vec3f not working - reason still unknown...
+        Mat_<Vec3b>::iterator it = frame_scaled.begin<Vec3b>();
+        // the color image to input should be in BGR order
+        for (unsigned j = 0; j < volChl; ++j)
+        {
+            //OpenCV read in frame as BGR format, by default, 
+            //thus need only deduct the mean value
+            data[c*volChl + j] = float((*it)[c]) - pixelMean[c];
+            it++;
+        }
+    }
+    delete [] data;
     return 0;
 }
 
