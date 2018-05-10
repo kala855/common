00d8fa25b6d27b875f66b6552a0e205a66d0318a
John Osorio <johnosorio@Johns-MacBook-Pro.local>
2018-05-10 09:28:51 -0500

    Some preprocessing was implemented. The program takes a video input and rescale and change the format depending on the number of channels

--- src/common.cpp
+++ src/common.cpp
@@ -1,7 +1,23 @@
 #include<stdio.h>
 #include <opencv2/opencv.hpp>
+
+static const int INPUT_W = 500;
+static const int INPUT_H = 375;
+
 using namespace cv;
 
+int preprocessFrame(Mat frame, Mat& frame_scaled){
+    
+    Size size(INPUT_W,INPUT_H);
+
+    if(frame.empty()) return -1;
+
+    if(frame.channels() == 4)
+        cvtColor(frame, frame, COLOR_BGRA2BGR);
+    resize(frame, frame_scaled, size);
+    
+    return 0;
+}
 
 int main(int argc, char **argv){
     if (argc!=2){
@@ -9,15 +25,16 @@ int main(int argc, char **argv){
     }
     VideoCapture capture(argv[1]);
     if(!capture.isOpened()){
-	printf("Video doesn't exists\n");
+        printf("Video doesn't exists\n");
     }
     namedWindow("frame",CV_WINDOW_AUTOSIZE);
     Mat frame;
+    Mat frame_scaled;
     for(;;){
-	capture>>frame;
-	if(frame.empty()) break;
-	imshow("frame",frame);
-	if(waitKey(0) == 27) break;
+        capture>>frame;
+        if (preprocessFrame(frame, frame_scaled)!=0) break;
+        imshow("frame",frame_scaled);
+        if(waitKey(0) == 27) break;
     }
     capture.release();
     destroyAllWindows();
