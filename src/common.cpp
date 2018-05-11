76482dbb1dac7e21ab958ec568ccf666a73f11a4
John Osorio <john@sirius.utp.edu.co>
2018-05-11 10:29:20 -0500

    Preparing the preprocess function to only generate the data pointer to be used by doInference function

--- src/common.cpp
+++ src/common.cpp
@@ -8,16 +8,13 @@ static const int N = 1;
 
 using namespace cv;
 
-int preprocessFrame(Mat frame, Mat& frame_scaled){
-    
+int preprocessFrame(Mat frame, Mat& frame_scaled, float* data){
+
     Size size(INPUT_W,INPUT_H);
 
-    //In this pointer we are going to put the info of each frame
-    //in order to process it and feed the FasterRCNN example 
-    float* data = new float[N*INPUT_C*INPUT_H*INPUT_W];
-    //Pixel mean used by FasterRCNN authors. Also in BGR order
-    float pixelMean[3] = { 102.9801f, 115.9465f, 122.7717f }; 
-    
+   //Pixel mean used by FasterRCNN authors. Also in BGR order
+    float pixelMean[3] = { 102.9801f, 115.9465f, 122.7717f };
+
     if(frame.empty()) return -1;
 
     if(frame.channels() == 4)
@@ -25,20 +22,19 @@ int preprocessFrame(Mat frame, Mat& frame_scaled){
     resize(frame, frame_scaled, size);
     Mat_<Vec3f>::iterator it;
     unsigned int volChl = INPUT_H*INPUT_W;
-    for (int c = 0; c < INPUT_C; ++c)                              
+    for (int c = 0; c < INPUT_C; ++c)
     {
         //Vec3f not working - reason still unknown...
         Mat_<Vec3b>::iterator it = frame_scaled.begin<Vec3b>();
         // the color image to input should be in BGR order
         for (unsigned j = 0; j < volChl; ++j)
         {
-            //OpenCV read in frame as BGR format, by default, 
+            //OpenCV read in frame as BGR format, by default,
             //thus need only deduct the mean value
             data[c*volChl + j] = float((*it)[c]) - pixelMean[c];
             it++;
         }
     }
-    delete [] data;
     return 0;
 }
 
@@ -53,12 +49,18 @@ int main(int argc, char **argv){
     namedWindow("frame",CV_WINDOW_AUTOSIZE);
     Mat frame;
     Mat frame_scaled;
+
+    //In this pointer we are going to put the info of each frame
+    //in order to process it and feed the FasterRCNN example
+    float* data = new float[N*INPUT_C*INPUT_H*INPUT_W];
+
     for(;;){
         capture>>frame;
-        if (preprocessFrame(frame, frame_scaled)!=0) break;
+        if (preprocessFrame(frame, frame_scaled, data)!=0) break;
         imshow("frame",frame_scaled);
         if(waitKey(0) == 27) break;
     }
+    delete [] data;
     capture.release();
     destroyAllWindows();
     return 0;
